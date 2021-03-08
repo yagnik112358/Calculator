f6dda96d5a63f2d06fbeddc5016c41952c4e1e7b
yagnik112358 <ybprajapati96@gmail.com>
2021-03-08 20:58:00 +0530

    Main updated.

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.calculator.Calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator obj = new Calculator();
+        System.out.println("Select options");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+        System.out.println("Kam thy gyu");
+    }
+}
